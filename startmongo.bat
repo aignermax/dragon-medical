ada1f4353b19269b6a3739c68ef2c5c541e0162a
Max Aigner <max.wolfgang.aigner@gmail.com>
2017-07-27 14:42:36 +0200

    automatically add Database User on Windows Machine

--- startmongo.bat
+++ startmongo.bat
@@ -1,7 +1,19 @@
+REM -> Set PATH Variable to mongoDB folder in programfiles!
 REM - Next step would be to add Jenkins Integration to Database for Staging-Database 
 REM (copy all Entries from production into staging database with ssh, probably host on Vultr)
 REM on vultr we could also use a docker but it's probably easiest to simply use the VM there
 REM Also we have to add Usercontrol, then
-if not exist "data\db" mkdir "data\db"
-mongod.exe --dbpath "data\db" 
-REM mongo.exe Dragon --eval "db.user.insert({email:'max_aigneraigner@web.de', name'tester' , password: '$2a$04$hKyOOOJPyiSc0ca2xNqCguHwUywRWZPn0P.7H4BbjqwNzH4zKww7u' });"
+if not exist "data\db" (
+    mkdir "data\db" 
+    call :addStandardUser
+) else (
+    START mongod.exe --dbpath "data\db"
+)
+
+goto:eof
+
+:addStandardUser
+    START mongod.exe --dbpath "data\db"
+    timeout 3
+    mongo.exe Dragon --eval "db.user.insert({email:'max_aigneraigner@web.de', name: 'tester' , password: '$2a$04$hKyOOOJPyiSc0ca2xNqCguHwUywRWZPn0P.7H4BbjqwNzH4zKww7u' });"
+EXIT /B 0
